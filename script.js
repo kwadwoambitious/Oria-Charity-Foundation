4ea37b82c7696f041c1f559be7e1d2959567e48b
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-04 13:33:23 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const mainMenu = document.querySelector("ul");
+const menuBar = document.querySelector(".menu-bar");
+const header = document.querySelector("header");
+      
+menuBar.addEventListener("click", () => {
+    menuBar.classList.toggle("active");
+    mainMenu.classList.toggle("active");
+    header.classList.toggle("active");
+});
+document.querySelectorAll(".menu-item").forEach(n => n.addEventListener("click", () => {
+    menuBar.classList.remove("active");
+    mainMenu.classList.remove("active"); 
+}));
+
+const main = document.querySelector('main');
+const sectionOneOptions = {
+  rootMargin: "-400px 0px 0px 0px"
+};
+const sectionOneObserver = new IntersectionObserver
+(
+  function(
+    entries, 
+    sectionOneObserver
+  ){
+    entries.forEach(entry => {
+      if(!entry.isIntersecting){
+        header.classList.add('scrolled');
+      }
+      else{
+        header.classList.remove('scrolled');
+      }
+    });
+  },
+  sectionOneOptions);
+sectionOneObserver.observe(main);
